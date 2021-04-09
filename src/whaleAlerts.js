ff9b7d0b06a139396ad1d43c8bec56d7d8e79351
Nathan Hotz <nhotz@dmsi.com>
2021-04-09 16:41:29 -0500

    fixed fixed datetime

--- src/whaleAlerts.js
+++ src/whaleAlerts.js
@@ -1,12 +1,12 @@
 const fetch = require("node-fetch");
 
-const TEN_SECONDS_AGO = Math.floor(new Date().getTime() / 1000 - 10);
 const ONE_DAY = 24 * 60 * 60 * 1000;
 const whaleTracker = new Map();
 
 const getWhaleAlerts = () => {
+  const tenSecondsAgo = Math.floor(new Date().getTime() / 1000 - 10);
   return new Promise(async (res) => {
-    const url = `https://api.whale-alert.io/v1/transactions?start=${TEN_SECONDS_AGO}&min_value=5000000`;
+    const url = `https://api.whale-alert.io/v1/transactions?start=${tenSecondsAgo}&min_value=5000000`;
     try {
       const resp = await fetch(url, {
         headers: {
